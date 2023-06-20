493736ff890b03e0dacd1d7734aad0e4abde6255
ls125781003 <125781003@qq.com>
2023-06-20 13:50:37 +0800

    Update dczs.js

--- dczs.js
+++ dczs.js
@@ -1,6 +1,6 @@
 @js:
 function init_duanPluin(){
-    let sel_title = ['配置选择','编辑解析','程序主页','更新插件','批量管理','使用说明'].map((it) => {
+    let sel_title = ['配置选择','编辑解析','程序主页','更新插件','批量管理','使用说明']。map((it) => {
         if(it==='配置选择'){
             return '🎯️‍️'+it;
         }else if(it==='更新插件'){
@@ -13,24 +13,24 @@ function init_duanPluin(){
             return '🍀'+it;
         }
     });
-    return $(sel_title,2).select(() => {
-        function compare(name) {
+    return $(sel_title,2)。select(() => {
+        function 对比(名字) {
             return function (it) {
                 return it === name;
             }
         }
-        if(/配置选择/.test(input)){
+        if(/配置选择/。test(input)){
             return require('https://ghproxy.com/https://raw.githubusercontent.com/ls125781003/dcbf/main/v/Route.js'),setupPages("设置", 1)
-        }else if(/更新/.test(input)){
-            return $('确定更新插件吗?').confirm(()=>{
+        }else if(/更新/。test(input)){
+            return $('确定更新插件吗?')。确认(()=>{
                 let base_search_js = 'hiker://files/rules/dzHouse/js/断插助手.js';
-                writeFile(base_search_js,fetch('https://ghproxy.com/https://raw.githubusercontent.com/ls125781003/dmtg/main/dczs.json'));
+                writeFile(base_search_js,fetch('https://ghproxy.com/https://raw.githubusercontent.com/ls125781003/dmtg/main/dczs.js'));
                 refreshPage(true);
                 return 'toast://已更新'
             })
-        }else if(/程序主页/.test(input)){
+        }else if(/程序主页/。test(input)){
             return "hiker://home@𝑀𝑦𝐹𝑖𝑒𝑙𝑑ᴰⁿ"
-        }else if(/批量管理/.test(input)){
+        }else if(/批量管理/。test(input)){
             return "file:///storage/emulated/0/Android/data/com.example.hikerview/files/Documents/cache/plglParse.html"
         }else{
               return require('https://ghproxy.com/https://raw.githubusercontent.com/ls125781003/dcbf/main/v/Route.js'),setupPages("编辑", 1)
